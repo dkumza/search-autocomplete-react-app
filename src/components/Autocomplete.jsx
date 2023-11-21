657531977940c872ad7c7ba5e9b1d7d43a46a7b2
Darius <dariuskumza@gmail.com>
2023-11-21 11:02:40 +0200

    show 10 matching items

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -2,7 +2,7 @@ export default function autoComplete({ cities, clicked }) {
    return (
       <ul className=" flex flex-col w-full divide-y">
          {/* from cities props display li ele to DOM */}
-         {cities.map((city, index) => (
+         {cities.slice(0, 10).map((city, index) => (
             <li
                // on click return city value
                onClick={() => clicked(city)}
